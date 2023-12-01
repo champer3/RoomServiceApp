bbf010b827e56026a599a4daa2c3d57bc4afbbe9
Emmanuel <idenyiemmanuel9@gmail.com>
2023-12-01 08:45:16 -0600

    adding more files
    adding the pills, and most searched layouts

---
+++ components/Pills/Pills.js
@@ -0,0 +1,25 @@
+import { Pressable, StyleSheet, Text, View } from "react-native"
+
+function Pill({text, type = 'pill'}){
+    const weight = type == 'pill' ? 'thin': 'bold'
+    const radius = type == 'pill' ? 50 : 25
+    return <Pressable style= {({pressed}) => pressed && styles.pressed}>
+        <View style={[styles.container, {borderRadius: radius, }]} ><Text style={[styles.text, {fontWeight: weight}]} >{text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()}</Text></View>
+    </Pressable>
+}
+
+export default Pill
+const styles= StyleSheet.create({
+    container: { backgroundColor: '#f9f3cf59',
+    marginVertical: 10, 
+    marginHorizontal: 10, 
+    padding: 17,
+    alignItems: "center",
+    },
+    pressed : {
+        opacity : 0.5
+    },
+    text: { fontSize: 16,
+            color: '#333333',
+            }
+})
---
+++ components/Search/Search.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native"
+import Pill from "../Pills/Pills"
+
+function Search({items}){
+
+    return <View style ={styles.container}>
+        {items.map((item, ind)=> <Pill key={ind} text={item}/>)}
+    </View>
+}
+
+export default Search
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    }
+})
