c90819862443437a24fbdcf3156e71b504077369
Emmanuel <idenyiemmanuel9@gmail.com>
2023-12-02 17:47:29 -0600

    Adding the input comp
    Finished all the input component and the usage found in the
    documentation

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+
+// import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
     // <PaperProvider >
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>New Code</Text>
-      <BoxItemCategory/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
     // </PaperProvider>
   );
 }
@@ -22,5 +24,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  holder : {
+    flexDirection: 'row',
+    width: 'auto',
+  }
   
 });
