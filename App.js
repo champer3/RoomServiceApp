e82552a4287492be3c3550a16010c77dfb5e224e
champer3 <okunolas21@gmail.com>
2023-12-04 08:54:03 -0600

    removed deliverymode import from app.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
-import DeliveryMode from "./components/DeliveryMode";
 
 export default function App() {
   return (
@@ -10,7 +9,6 @@ export default function App() {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>New Code</Text>
         <StatusBar style="auto" />
-        <DeliveryMode mode="Fast" time="10 - 15" />
       </View>
     </PaperProvider>
   );
