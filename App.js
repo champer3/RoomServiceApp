a4f4992c2de21915174eab5d4efff3810d9f0cd2
Emmanuel <idenyiemmanuel9@gmail.com>
2023-12-07 12:37:06 -0600

    made changes to code

--- App.js
+++ App.js
@@ -3,16 +3,50 @@ import { StyleSheet, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import {Input} from './components/Inputs/Input'
 import Product from "./components/Product/Product";
+import BoxItemCategory from "./components/Category/BoxItemCategory";
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <View style={{flexDirection: 'row', width: 410}}>
-          <Product/>
-          <Product/>
-          </View>
+        <BoxItemCategory items={[{text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         {text: 'Snacks', image: require('./assets/dsBuffer.bm.png')},
+         ]}/>
+        <Product/>
       </View>
     </PaperProvider>
   );
--- components/Item/BoxedItem.js
+++ components/Item/BoxedItem.js
@@ -7,7 +7,7 @@ function BoxedItem({ text, image }) {
     Poppins_700Bold,
   });
   return (
-    <Pressable style={({ pressed }) => pressed && { opacity: 0.5 }}>
+   
       <View style={styles.container}>
         <View style={styles.imageContainer}>
           <Image style={styles.image} source={image} />
@@ -18,7 +18,6 @@ function BoxedItem({ text, image }) {
           </Text>
         </View>
       </View>
-    </Pressable>
   );
 }
 
@@ -36,11 +35,10 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "rgba(0,0,0,0.05)",
     borderRadius: 10,
-    height: 135,
     marginHorizontal: 20,
     marginTop: 20,
     flexDirection: "row",
-    width: 250,
+    width: '42%',
   },
   image: { maxWidth: 100, height: 130, resizeMode: "contain" },
   text: { fontSize: 20, color: "rgba(0,0,0,0.8)" },
